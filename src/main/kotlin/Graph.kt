73efaf19e83c18a2e7869f191d177effeb8927b0
ProgramNotWorking <Ve7ter@yandex.ru>
2024-05-19 23:15:28 +0300

    idk what i am doing wrong

---
+++ src/main/kotlin/Graph.kt
@@ -0,0 +1,124 @@
+package org.example
+
+import org.jfree.chart.ChartFactory
+import org.jfree.chart.ChartPanel
+import org.jfree.chart.JFreeChart
+import org.jfree.data.xy.XYSeries
+import org.jfree.data.xy.XYSeriesCollection
+import javax.swing.JFrame
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sin
+
+object Graph {
+
+    // dataset for graph
+    private val dataset = XYSeriesCollection()
+    private val mainSeries = XYSeries("t")
+    private val functionLine = XYSeries("Function line")
+    private val constLine = XYSeries("u(t)")
+
+    // values
+    private var h: Int = 0
+    private var u0: Int = 0
+    private var t0: Int = 0
+    private var t: Int = 0
+    private var trident: Int = 0
+    private var b: Int = 0
+
+    // value for graph length
+    private const val T = 40.0
+
+    // function line
+    private fun function(t: Double): Double = 3 * sin(t)
+
+    fun drawGraph() {
+        askForData()
+        initGraphWithValues()
+        println("Производная psi: ${findTridentDerivative()}")
+    }
+
+    private fun initGraphWithValues() {
+        for (t in 0..(T * 10).toInt() step 1) {
+            val point = (t / 10).toDouble()
+            if (isFunctionIncreasing(
+                    function = { x: Double -> 3 * sin(x) },
+                    intervalEnd = t.toDouble()
+                )
+            ) {
+                constLine.add(point, max(u0, bOfT(t)))
+            } else {
+                constLine.add(point, min(u0, bOfT(t) + h))
+            }
+            functionLine.add(point, function(t.toDouble()))
+            mainSeries.add(point, 0)
+        }
+
+        dataset.addSeries(functionLine)
+        dataset.addSeries(constLine)
+        dataset.addSeries(mainSeries)
+
+        val chart: JFreeChart = ChartFactory.createXYLineChart(
+            "График",
+            "t",
+            "y",
+            dataset
+        )
+
+        val frame = JFrame("График")
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.add(ChartPanel(chart))
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.isVisible = true
+    }
+
+    private fun askForData() {
+        println("Введите h: ")
+        h = readLine()?.toInt() ?: 1
+        println("Введите u0: ")
+        u0 = readLine()?.toInt() ?: 3
+        println("Введите t0: ")
+        t0 = readLine()?.toInt() ?: 0
+        println("Введите t: ")
+        t = readLine()?.toInt() ?: 0
+        println("Введите psi: ")
+        trident = readLine()?.toInt() ?: 1
+        println("Введите sigma: ")
+        // sigma
+        b = readLine()?.toInt() ?: 0
+    }
+
+    // Fun for derivative (just print it at end)
+    private fun findTridentDerivative(): Int =
+        if (trident > b && trident < (b + h)) {
+            0
+        } else if (trident == b) {
+            max(0, b)
+        } else if (trident == b + h) {
+            max(0, b)
+        } else {
+            -1
+        }
+
+    // Sigma of t value i guess
+    private fun bOfT(t: Int) = kotlin.math.abs(kotlin.math.abs(t) - 2)
+
+    private fun isFunctionIncreasing(
+        function: (Double) -> Double,
+        intervalStart: Double = 0.0,
+        intervalEnd: Double
+    ): Boolean {
+        if (intervalEnd <= intervalStart) return false // Интервал пустой или некорректный
+
+        var lastValue = function(intervalStart)
+        for (i in intervalStart.toInt() until intervalEnd.toInt()) {
+            val currentValue = function(i.toDouble())
+            if (currentValue >= lastValue) return false // Значение не увеличивается
+            lastValue = currentValue
+        }
+
+        return true // Все значения увеличиваются
+    }
+
+}
